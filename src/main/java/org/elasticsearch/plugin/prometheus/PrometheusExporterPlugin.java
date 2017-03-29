6acdab912bf8bcf6f3f9bf8d2bc7334be1b833e2
Vincent VAN HOLLEBEKE <vincent@compuscene.org>
2017-03-29 15:26:08 +0200

    Upgrade to ElasticSearch 5.3.0

--- src/main/java/org/elasticsearch/plugin/prometheus/PrometheusExporterPlugin.java
+++ src/main/java/org/elasticsearch/plugin/prometheus/PrometheusExporterPlugin.java
@@ -1,18 +1,24 @@
 package org.elasticsearch.plugin.prometheus;
 
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.IndexScopedSettings;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsFilter;
 import org.elasticsearch.index.IndexModule;
 import org.elasticsearch.plugins.ActionPlugin;
 import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestHandler;
 import org.elasticsearch.rest.prometheus.RestPrometheusMetricsAction;
 
+import java.util.Arrays;
 import java.util.List;
-
-import static java.util.Collections.singletonList;
+import java.util.function.Supplier;
 
 public class PrometheusExporterPlugin extends Plugin implements ActionPlugin {
 
@@ -29,8 +35,10 @@ public class PrometheusExporterPlugin extends Plugin implements ActionPlugin {
     }
 
     @Override
-    public List<Class<? extends RestHandler>> getRestHandlers() {
-        return singletonList(RestPrometheusMetricsAction.class);
+    public List<RestHandler> getRestHandlers(Settings settings, RestController restController, ClusterSettings clusterSettings, IndexScopedSettings indexScopedSettings, SettingsFilter settingsFilter, IndexNameExpressionResolver indexNameExpressionResolver, Supplier<DiscoveryNodes> nodesInCluster) {
+        return Arrays.asList(
+                new RestPrometheusMetricsAction(settings, restController)
+        );
     }
 
 }
