31e9f80d3896a0636258bb4b91fc4eb49411be14
Geoffrey Thomas <geoffrey.thomas@metaswitch.com>
2022-03-29 13:52:10 +0100

    Some references have moved

--- src/main/java/org/elasticsearch/action/NodePrometheusRequestBuilder.java
+++ src/main/java/org/elasticsearch/action/NodePrometheusRequestBuilder.java
@@ -18,7 +18,7 @@
 package org.elasticsearch.action;
 
 import org.elasticsearch.action.support.master.MasterNodeReadOperationRequestBuilder;
-import org.elasticsearch.client.ElasticsearchClient;
+import org.elasticsearch.client.internal.ElasticsearchClient;
 
 /**
  * Request builder class for Prometheus Exporter plugin.
--- src/main/java/org/elasticsearch/rest/prometheus/RestPrometheusMetricsAction.java
+++ src/main/java/org/elasticsearch/rest/prometheus/RestPrometheusMetricsAction.java
@@ -27,7 +27,7 @@ import org.compuscene.metrics.prometheus.PrometheusMetricsCollector;
 import org.compuscene.metrics.prometheus.PrometheusSettings;
 import org.elasticsearch.action.NodePrometheusMetricsRequest;
 import org.elasticsearch.action.NodePrometheusMetricsResponse;
-import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.client.internal.node.NodeClient;
 import org.elasticsearch.common.network.NetworkAddress;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Settings;
